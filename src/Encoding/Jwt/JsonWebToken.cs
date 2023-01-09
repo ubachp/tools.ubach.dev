2c443eb8ba36d2bf4a7053f9f00c26c84c9f7c02
Paulo Ubach <paulo.ubach@gmail.com>
2023-01-08 21:11:33 -0500

    - New Jwt project - New website

---
+++ src/Encoding/Jwt/JsonWebToken.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ubach.Encoding.Jwt;
+public class JsonWebToken
+{
+    public JsonWebToken(string jwt)
+    {
+        var parts = jwt.Split('.');
+        if (parts.Length != 3)
+        {
+            throw new ArgumentException($"{nameof(jwt)} is malformed. It don't have 3 parts");
+        }
+
+        Header = parts[0];
+        Payload= parts[1];
+        Signature = parts[2];
+    }
+
+    public string Header { get; set; }
+    public string Payload { get; set; }
+    public string Signature { get; set; }
+}
